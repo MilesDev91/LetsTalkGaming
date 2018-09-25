caca34460062350c5ff3faba38273ac59da77bb5
Miles Coffin <micoffin91@gmail.com>
2018-09-25 04:37:24 -0400

    Add project files.

---
+++ TalkGaming/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TalkGaming.MvcApplication" Language="C#" %>
---
+++ TalkGaming/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TalkGaming.Startup))]
+namespace TalkGaming
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
